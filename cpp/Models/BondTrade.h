8df25b7166afcfef61b37dd28b0beedbceda06f9
Oakley Brunt <oakleybrunt72@gmail.com>
2026-07-20 22:51:34 +0100

    Add processing of strings and tradeType setter method

--- cpp/Models/BondTrade.h
+++ cpp/Models/BondTrade.h
@@ -9,18 +9,17 @@ public:
     static constexpr const char* GovBondTradeType = "GovBond";
     static constexpr const char* CorpBondTradeType = "CorpBond";
     
-    BondTrade(const std::string& tradeId, const std::string& tradeType = GovBondTradeType) 
-        : tradeType_(tradeType) {
+    BondTrade(const std::string& tradeId, const std::string& tradeType = GovBondTradeType) {
         if (tradeId.empty()) {
             throw std::invalid_argument("A valid non null, non empty trade ID must be provided");
         }
         tradeId_ = tradeId;
+        tradeType_ = tradeType;
     }
     
+    // Protected not private
     std::string getTradeType() const override { return tradeType_; }
-    
-private:
-    std::string tradeType_;
+
 };
 
 #endif // BONDTRADE_H
